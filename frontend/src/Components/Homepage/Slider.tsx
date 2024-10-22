e8a15fef7101069459d698ef32ab4422bc0cc7f9
Miriam Ciurea <miriamciurea777@icloud.com>
2024-10-22 16:43:59 +0300

    responsive loading

--- frontend/src/Components/Homepage/Slider.tsx
+++ frontend/src/Components/Homepage/Slider.tsx
@@ -46,8 +46,10 @@ const Slider: React.FC = () => {
 
         <ul ref={sliderRef} className={styles.highwayLane} style={{ transform: `translateX(${scrollPosition}px)` }}>
           {sliderItems.map((item, index) => (
-            <li key={index} className={styles.highwayCar}>
-              <div className="w-[200px] h-[200px] object-contain"
+            <li key={index} className={`${styles.highwayCar} text-[11rem]
+                                        md:text-[17rem]`}>
+              <div className=" object-contain w-[100px] h-[100px]
+                              md:w-[200px] md:h-[200px]"
               style = {{
                 backgroundImage: `url(${item.img})`,
                 backgroundSize: "contain",
@@ -66,67 +68,3 @@ const Slider: React.FC = () => {
 };
 
 export default Slider;
-
-// import React, { useEffect, useState } from 'react';
-// import styles from './Slider.module.css';
-
-// const Slider: React.FC = () => {
-//   const items = [
-//     { text: "DESIGN", img: "public/images/1.png" },
-//     { text: "DEVELOP", img: "public/images/2.png" },
-//     { text: "DEPLOY", img: "public/images/3.png" },
-//     { text: "TRANSFORM", img: "public/images/4.png" },
-//     { text: "MANAGE", img: "public/images/5.png" },
-//   ];
-
-//   const sliderItems = [...items, ...items]; // Duplicate items for seamless loop
-//   const [isReversed, setIsReversed] = useState(false); // Control direction of scroll
-
-//   // Effect to change direction based on scroll
-//   useEffect(() => {
-//     let lastScrollTop = 0;
-
-//     const handleScroll = () => {
-//       const scrollTop = window.scrollY || document.documentElement.scrollTop;
-
-//       if (scrollTop > lastScrollTop) {
-//         setIsReversed(false); // Scrolling down, use normal scroll (left)
-//       } else {
-//         setIsReversed(true); // Scrolling up, reverse the scroll (right)
-//       }
-
-//       lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
-//     };
-
-//     window.addEventListener('scroll', handleScroll);
-
-//     return () => window.removeEventListener('scroll', handleScroll);
-//   }, []);
-
-//   return (
-//     <div className={styles.sliderContainer}>
-//       <div className={styles.highwayBarrier}>
-//         <ul
-//           className={`${styles.highwayLane} ${isReversed ? styles.reverse : ''}`}
-//         >
-//           {sliderItems.map((item, index) => (
-//             <li key={index} className={styles.highwayCar}>
-//               <div
-//                 className="w-[200px] h-[200px] object-contain"
-//                 style={{
-//                   backgroundImage: `url(${item.img})`,
-//                   backgroundSize: "contain",
-//                   backgroundRepeat: "no-repeat",
-//                   backgroundPosition: "center",
-//                 }}
-//               ></div>
-//               <div>{item.text}</div>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Slider;
