c195dfcfd2f30d51eb206a9affcea85d9eeafa58
Miriam Ciurea <miriamciurea777@icloud.com>
2024-10-29 19:58:35 +0200

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
 app.use('/api', contactRoutes);
