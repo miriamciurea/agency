5e4ca9a530886090120bedb6837e9b70260038ba
Miriam Ciurea <miriamciurea777@icloud.com>
2024-10-30 13:20:54 +0200

    homepage is better

--- frontend/src/Components/Homepage/Hero.tsx
+++ frontend/src/Components/Homepage/Hero.tsx
@@ -1,25 +1,29 @@
 import { useRef } from 'react';
 import useMouseMove from '../hooks/useMouseMove';
 import 'animate.css';
+import { useNavigate } from 'react-router-dom';
 
 const Hero = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const navigate = useNavigate();
 
   // Apply the useMouseMove hook to the button within the container
   useMouseMove(containerRef, buttonRef);
 
   return (
     <section ref={containerRef} className="p-[7%] gap-[20%]
-                                            grid md:grid-cols-[1fr_2fr] items-center md:p-[2%] md:mt-[15%] md:gap-0">
+                                            grid md:grid-cols-[1fr_2fr] items-center md:p-[2%] md:mt-[12%] md:gap-0">
       <div className="flex flex-col gap-[20px]
                       md:h-[100%] md:w-[100%] md:justify-end animate__animated animate__fadeInLeft">
         <div className="md:pr-[20%] md:text-[1.3rem]">
-          Pick a plan, submit a job request, and your project will kickoff within 24 hours.
+        Submit a job request, and we’ll get back to you as soon as possible to start your project.
+
         </div>
 
         <div className="">
-          <button ref={buttonRef} className="button">Explore our services</button>
+          <button ref={buttonRef} className="button" onClick={() => navigate('/services')}>Explore our services</button>
         </div>
       </div>
 
